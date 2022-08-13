ed4b121ca50305681339733b165ad6400d243ea7
ErMapsh <maheshmestri73@gmail.com>
2022-08-13 19:45:30 +0530

    data types and var

---
+++ Tutorials/Tutorials/src/com/_1ermpash/Main.java
@@ -0,0 +1,22 @@
+package com._1ermpash;//Package is used to group related classes, think of it as a folder in a file dir
+
+public class Main {
+    public static void main(String[] args) {
+    // The main() method is entry point in application
+        System.out.println("Hello world!");
+    }
+}
+
+
+/*
+    java is made up of class and functions,
+    explanation:
+    class is made up of functions. ie Home repair
+    functions made up to perform specific task. ie plumber, painter, carpenter
+
+    public static void main -->  return void, we can access main public function from anywhere
+
+    Naming Convetions :
+    1.For classes we use PascleConvention,
+    2.For function we use camleCaseConvetion
+*/
---
+++ Tutorials/Tutorials/src/com/_2DataTypesAndVar/DataTypes.java
@@ -0,0 +1,42 @@
+package com._2DataTypesAndVar;
+
+public class DataTypes {
+    public static void main(String[] args){
+        System.out.println("----Java DataTypes----- ");
+        int digit = 8;
+        float floatValue = 1.3f;
+        char character = 'c';
+        boolean bln = true;
+        long _long = 15000000000L;
+        double double2 = 19.99d;
+        byte binary = 100;
+        short int$ = 5000;
+        int num2 = 4;
+
+        System.out.print("sum fo 3 Digit is ");
+        System.out.println(digit+int$+num2);
+    }
+}
+
+/* There Tow type of date types are there:
+1.Primitive dataTypes
+2.Non-Primitive DataTypes (Derived)
+
+1]--> in primitive data types there following data types are include:
+int---> 4 bytes,
+float--> 4 bytes,
+char--> 2 bytes,
+bool--> size depend on JVM,
+long--> 8bytes,
+double-->8 bytes,
+short--> 2 bytes,
+byte: store binary values between 0(00000000) and 255(1111111).
+
+
+# Rules for Var:
+1.var not start with digit
+2.no space available in var
+3.var name is case-sensitive example ermapsh and ErMapsh
+4.should not a keyword
+5.can contain alphabet, char, _ char and digit if the other conditions are met.
+*/
