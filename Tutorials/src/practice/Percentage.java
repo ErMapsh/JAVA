aaf62f89f3d6453db9b5a17b49ac0b73569778d5
ErMapsh <maheshmestri73@gmail.com>
2022-08-28 13:47:55 +0530

    percentage

---
+++ Tutorials/src/practice/Percentage.java
@@ -0,0 +1,26 @@
+package practice;
+import java.util.Scanner;
+
+public class Percentage {
+    public static void main(String[] args){
+        System.out.println("----Percentage----");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter First subject marks: ");
+        int a = sc.nextInt();
+        System.out.print("Enter Second subject marks: ");
+        int b = sc.nextInt();
+        System.out.print("Enter Third subject marks: ");
+        int c = sc.nextInt();
+        System.out.print("Enter Fourth subject marks: ");
+        int d = sc.nextInt();
+        System.out.print("Enter Fifth subject marks: ");
+        int e = sc.nextInt();
+
+        double percentage = ((a+b+c+d+e)/500d) * 100d;
+        System.out.print("Percentage: " + percentage);
+
+
+    }
+}
+
