4a0a6a4eec54b5070540eb3befbf175fba3cc4b9
ErMapsh <maheshmestri73@gmail.com>
2022-08-28 13:16:25 +0530

    user Input

---
+++ Tutorials/src/com/_4UserInput/userInput.java
@@ -0,0 +1,28 @@
+package com._4UserInput;
+import java.util.Scanner;
+
+
+public class userInput {
+    public static void main(String[] args){
+        /* here Scanner is class that have utilities to get input */
+        System.out.println("----User Input----");// we can also write short-form sout for System.out.print
+
+        Scanner sc = new Scanner(System.in);
+
+        /*
+        System.out.print("Enter number 1: ");
+        int a = sc.nextInt();
+        System.out.print("Enter number 2: ");
+        int b = sc.nextInt();
+        System.out.print("Enter the string: ");
+        String str = sc.next();
+
+        int sum = a + b;
+        System.out.println(str+" "+sum);
+        */
+
+        System.out.println(sc.hasNextInt());
+
+        System.out.println(sc.nextLine());
+    }
+}
