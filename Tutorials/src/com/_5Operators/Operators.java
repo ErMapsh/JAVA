c8ab80968b2e369328e327151e0674135da2a0db
ErMapsh <maheshmestri73@gmail.com>
2022-08-29 13:11:58 +0530

    operator

---
+++ Tutorials/src/com/_5Operators/Operators.java
@@ -0,0 +1,14 @@
+package com._5Operators;
+
+public class Operators {
+    public static void main(String[] args) {
+        System.out.println("---Operators---");
+        System.out.println("\n1.Arithmetic Operator---> +, -, *, /, %, --, ++");
+        System.out.println("\n2.Assignment Operator---> =, +=, -=, *=, /=");
+        System.out.println("\n3.Comparison Operator---> ==, !=, <, >, <=, >=");
+        System.out.println("\n4.Logical Operator---> &&, ||, !");
+        System.out.println("\n5.Bitwise Operator---> &, |");
+    }
+}
+
+/* Arithmetic operators cannot work with booleans, % operator can work on float and double */
