c9bfb5b529b44e21375943058b29b0779281a874
ermapsh by pc <maheshmestri12@gmail.com>
2022-09-07 14:47:01 +0530

    introduction to oops

---
+++ Tutorials/src/_8OOPS/_1oops.java
@@ -0,0 +1,16 @@
+package _8OOPS;
+
+public class _1oops {
+    public static void main(String[] args) {
+        /* OOPS---> object oriented Programming 
+           normally we write a code that called procedure oriented programming(without using obect oriented programming)
+
+            solving a problem by creating a object is one of the most popular best approches in programming, thats called oop
+            
+            what is object?
+            object is instance of class(copy of class), we can use class methods, class attribute after allocating/creating a object.
+        */
+        System.out.println("----Introduction of OOPS----");
+        
+    }
+}
