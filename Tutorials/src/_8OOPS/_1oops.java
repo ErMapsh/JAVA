a54d23a7646ff81add92821888a8a0cf2ef3673d
ermapsh by pc <maheshmestri12@gmail.com>
2022-09-07 16:15:49 +0530

    terminologies in oops

--- Tutorials/src/_8OOPS/_1oops.java
+++ Tutorials/src/_8OOPS/_1oops.java
@@ -10,7 +10,23 @@ public class _1oops {
             what is object?
             object is instance of class(copy of class), we can use class methods, class attribute after allocating/creating a object.
         */
-        System.out.println("----Introduction of OOPS----");
         
+        System.out.println("----Introduction of OOPS----");
+        /* 
+         Terminologies 
+         1.Astraction ---> hiding the internal detailes (use it , dont think about it how its work) ie. android development
+         
+         2.Encapsulation ---> the act of putting various components together (in capsual) like class ie. Laptop, car
+         overall encapsulation means that the hiding a data can be hidden from the users
+
+         3. Inheritance ----> The act of deriving new thing from existing thing.  ie.phone --> smarthphone      
+         DRY ===> In inheritance we use dry thing
+         
+         4. Polymorphism ----> one thing many form
+            ie. smartphone ----> phone
+                smartphone ----> calculater
+                smartphone -----> camera
+                smartphone -----> diary
+        */
     }
 }
