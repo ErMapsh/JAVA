5da47fe255a115e590d49b3bb81bb36c8f4b6045
ErMapsh <maheshmestri73@gmail.com>
2022-09-09 00:22:53 +0530

    dynamic method distpatch

--- Tutorials/src/_10Inheritance/_5DynamicMethodDispatch.java
+++ Tutorials/src/_10Inheritance/_5DynamicMethodDispatch.java
@@ -1,7 +1,39 @@
 package _10Inheritance;
 
+class Phone{
+    public void showTime(){
+        System.out.println("Time is 8 am");
+    }
+    public void on(){
+        System.out.println("Turning on Phone...");
+    }
+}
+
+class SmartPhone extends Phone{
+    public void music(){
+        System.out.println("Playing music...");
+    }
+    public void on(){
+        System.out.println("Turning on SmartPhone...");
+    }
+}
+
 public class _5DynamicMethodDispatch {
     public static void main(String[] args) {
-        
+
+        // ---normally this is allowed---
+        // Phone obj = new Phone(); // Allowed
+        // SmartPhone smobj = new SmartPhone(); // Allowed
+        // obj.name();
+
+        Phone obj = new SmartPhone(); // Yes it is allowed
+        // SmartPhone obj2 = new Phone(); // Not allowed
+
+        // smartphone is made up from phone
+        obj.showTime();
+        obj.on();
+        // obj.music(); // Not Allowed
+
+
     }
 }
